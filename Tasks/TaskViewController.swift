8253f29870b2a529a92f08441285fb146a4c4c85
kom3ta <109717804+kom3ta@users.noreply.github.com>
2022-07-28 20:49:50 +0500

    Added tasks detail

--- Tasks/TaskViewController.swift
+++ Tasks/TaskViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    
+    var task: String?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "delete", style: .done, target: self, action: #selector(deleteTask))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func deleteTask(){
+//
+//        let newCount = count - 1
+//
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task\(currentPosition)")
+        
     }
-    */
+
 
 }
--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -64,6 +64,13 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task = tasks[indexPath.row
+        ]
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
 }
 extension ViewController: UITableViewDataSource {
